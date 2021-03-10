7395b786c5e89b84b0ec74934ada7a7bbd7b195f
Sandra Juárez <68796014+SandGG@users.noreply.github.com>
2021-03-10 13:45:37 -0600

    add defer and change close method

--- main.go
+++ main.go
@@ -3,28 +3,49 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"log"
 	"strconv"
 
 	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
-	var db, _ = sql.Open("sqlite3", "./data.db")
+	var db, errOpen = sql.Open("sqlite3", "./data.db")
+	defer db.Close()
+	if errOpen != nil {
+		log.Fatal(errOpen)
+	}
+
 	//Prepare: Prepare a sentence and I can use it a lot of times
 	//Close statement when it not use any more
-	var statement, _ = db.Prepare("CREATE TABLE IF NOT EXISTS people (id INTEGER PRIMARY KEY, firstname TEXT, lastname TEXT)")
-	//Executes a query (no returns rows)
-	statement.Exec()
-	statement, _ = db.Prepare("INSERT INTO people (firstname, lastname) VALUES (?, ?)")
+	var statement, err = db.Prepare("CREATE TABLE IF NOT EXISTS people (id INTEGER PRIMARY KEY, firstname TEXT, lastname TEXT)")
+	defer statement.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	statement.Exec() //Executes a query (no returns rows)
+
+	statement, err = db.Prepare("INSERT INTO people (firstname, lastname) VALUES (?, ?)")
+	defer statement.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
 	statement.Exec("Marco", "Diaz")
-	statement, _ = db.Prepare("INSERT INTO people (firstname, lastname) VALUES (?, ?)")
+
+	statement, err = db.Prepare("INSERT INTO people (firstname, lastname) VALUES (?, ?)")
+	defer statement.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
 	statement.Exec("Ana", "Gomez")
 
-	//Close statement
-	statement.Close()
-
 	//Executes a query (returns rows)
-	var rows, _ = db.Query("SELECT id, firstname, lastname FROM people")
+	var rows, errSelect = db.Query("SELECT id, firstname, lastname FROM people")
+	defer rows.Close()
+	if errSelect != nil {
+		log.Fatal(errSelect)
+	}
+
 	var id int
 	var firstname string
 	var lastname string
